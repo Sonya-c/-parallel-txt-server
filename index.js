cab748cc16b125581fd77f8807435eaa4c095352
DavidCB <70921381+DavidC031@users.noreply.github.com>
2021-11-24 14:16:04 -0500

    Update index.js

--- index.js
+++ index.js
@@ -20,9 +20,32 @@ server.listen(PORT, () => {
 });
 
 io.on('connection', (socket) => {
+    console.log("Conexion establecida",socket.id);
+    socket.on('login', ({ name, room }, callback) => {
+        const { user, error } = addUser(socket.id, name, room)
+        if (error) return callback(error)
+        socket.join(user.room)
+        console.log("Conexion establecida",socket.id,name, room);
+        socket.in(room).emit('notification', { title: 'Someone\'s here', description: `${user.name} just entered the room` })
+        io.in(room).emit('users', getUsers(room))
+        callback()
+    })
     socket.emit('connection', null);
     
     socket.on('send-message', message => {
         io.emit('message', message);
     });
-});
+    socket.on('sendMessage', message => {
+        const user = getUser(socket.id)
+        io.in(user.room).emit('message', { user: user.name, text: message });
+    })
+
+    socket.on("disconnect", () => {
+        console.log("User disconnected");
+        const user = deleteUser(socket.id)
+        if (user) {
+            io.in(user.room).emit('notification', { title: 'Someone just left', description: `${user.name} just left the room` })
+            io.in(user.room).emit('users', getUsers(user.room))
+        }
+    })
+})
